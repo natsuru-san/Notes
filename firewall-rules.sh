33a84f1ecb9fdd0431f513e9ba44e791faf25cc7
natsuru <natsuru-san@mail.com>
2026-03-27 12:39:53 +0300

    set folder

--- firewall-rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-apt update && apt upgrade && apt install build-essential wget socat ufw -y
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow 22
-ufw allow 11443
-ufw enable
